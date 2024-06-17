079993cbe108aa5c972296fd52cae237d8af3749
alexey-ryabkov <xelaris@ya.ru>
2024-06-17 23:57:01 +0300

    chore: add postcss

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  syntax: "postcss-scss",
+  plugins: [
+    require("postcss-advanced-variables"),
+    require("postcss-nested"),
+    require("postcss-utilities"),
+    require("postcss-short"),
+    require("postcss-preset-env")({
+      features: {
+        "nesting-rules": false,
+        "media-query-ranges": true,
+        "custom-media-queries": true,
+      },
+    }),
+    require("autoprefixer"),
+  ],
+};
